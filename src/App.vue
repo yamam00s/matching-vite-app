91bfbe550daedb9bcf8c2908cb018af2efde9966
yamam00s <s.yamamo0327@gmail.com>
2020-09-28 22:06:05 +0900

    軽微な修正

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Home from './pages/Home.vue'
+import Home from '@/pages/Home.vue'
 
 export default defineComponent({
   name: 'App',
