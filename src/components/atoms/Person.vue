906fb9626b8ece5aa98952b96d002b543ebf5ede
yamam00s <s.yamamo0327@gmail.com>
2020-09-28 22:07:08 +0900

    create Person Component

---
+++ src/components/atoms/Person.vue
@@ -0,0 +1,42 @@
+<template>
+  <img :src="`${imagePathName}/${person.image}`" />
+  <p>{{ person.name }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, PropType } from 'vue'
+import { Person } from '@/services/models/person'
+
+type Props = {
+  person: Person
+  isMale: boolean
+}
+
+export default defineComponent({
+  name: 'Person',
+  props: {
+    person: {
+      type: Object as PropType<Person>,
+      default() {
+        return {}
+      },
+    },
+    isMale: {
+      type: Boolean,
+      default() {
+        return true
+      },
+    },
+  },
+  setup(props: Props) {
+    const imagePathName = computed(
+      () =>
+        `@/assets/person/${props.isMale ? 'male' : 'female'}/${
+          props.person.image
+        }`
+    )
+
+    return imagePathName
+  },
+})
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,23 +1,27 @@
 <template>
   <Suspense>
     <ul>
-      <li v-for="preson in persons" :key="preson.id">
-        {{ preson.name }}
+      <li v-for="person in persons" :key="person.id">
+        <Person :person="person" :is-male="isMale" />
       </li>
     </ul>
   </Suspense>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue'
+import { defineComponent, ref } from 'vue'
 import axios from 'axios'
-import { Person } from '../services/models/person'
+import { Person as PersonType } from '@/services/models/person'
+import Person from '@/components/atoms/Person.vue'
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    Person,
+  },
   setup() {
     const isMale = ref<boolean>(true)
-    const persons = ref<Person[]>([])
+    const persons = ref<PersonType[]>([])
 
     const getPersons = async () => {
       let endPoint: string
@@ -26,11 +30,11 @@ export default defineComponent({
       } else {
         endPoint = '/src/static/json/females.json'
       }
-      const res = await axios.get<Person[]>(endPoint)
+      const res = await axios.get<PersonType[]>(endPoint)
       persons.value = res.data
     }
 
-    onMounted(getPersons)
+    getPersons()
 
     return {
       persons,
