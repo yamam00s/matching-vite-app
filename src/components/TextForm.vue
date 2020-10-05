92c361951494e179398023239bfcee5f7cc486fd
yamam00s <s.yamamo0327@gmail.com>
2020-10-05 09:29:27 +0900

    create TextFrom

---
+++ src/components/TextForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="text-form">
+    <input v-model="inputText" type="text" />
+    <button type="button" @click="submitText">{{ buttonText }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, SetupContext } from 'vue'
+
+type Props = {
+  buttonText: string
+}
+
+export default defineComponent({
+  name: 'TextForm',
+  props: {
+    buttonText: {
+      type: String,
+      default() {
+        return '検索'
+      },
+    },
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  setup(props: Props, context: SetupContext) {
+    const inputText = ref<string>('')
+    const submitText = () => {
+      context.emit(`'emit-text'`, inputText)
+    }
+
+    return {
+      inputText,
+      submitText,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.text-form {
+  display: flex;
+
+  input {
+    border-radius: 5px;
+  }
+
+  button {
+    padding: 0px 30px;
+    background: #fff;
+    border-radius: 5px;
+  }
+}
+</style>
