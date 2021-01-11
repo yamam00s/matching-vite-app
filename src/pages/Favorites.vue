61b5841ca4f6b3e3434bebda1706329a222a0ef1
yamam00s <s.yamamo0327@gmail.com>
2021-01-12 08:45:41 +0900

    create Favoritesコンポーネント

---
+++ src/pages/Favorites.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li v-for="favorite in favorites" :key="favorite.id">
+      {{ favorite.name }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { usePersons } from '@/composables/usePersons'
+import { useFavoritesStore } from '@/composables/useFavorites'
+// import { Person as PersonType } from '@/services/models/person'
+
+export default defineComponent({
+  name: 'Favorites',
+  setup() {
+    const { displayPersons } = usePersons()
+    const { favorites } = useFavoritesStore()
+
+    return {
+      favorites,
+      displayPersons,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
